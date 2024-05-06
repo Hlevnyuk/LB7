bf600b9c53fde20dde488e3bf768d43ebb549535
Артём <lenovouser2010u@gmail.com>
2024-05-06 23:33:35 +0300

    New project +

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
+import pymongo
 from pymongo import MongoClient
+import random
 
-client_local = MongoClient('localhost', 27017)
-local_db = client_local['my_local_database']
-local_collection = local_db['my_collection']
+# З'єднання з MongoDB
+client = MongoClient('localhost', 27017)
+db = client['test_database']
 
-username = "Artem"
-password = "artemovich"
-cluster_name = "Cluster0"
-atlas_uri = f"mongodb+srv://Artem:artemovich@cluster0.6zltqqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client_atlas = MongoClient(atlas_uri)
-atlas_db = client_atlas['my_atlas_database']
-atlas_collection = atlas_db['my_collection']
+# Створення колекцій
+collection1 = db['collection1']
+collection2 = db['collection2']
 
-for document in local_collection.find():
-    atlas_collection.insert_one(document)
+# Функція для створення випадкового _id
+def generate_random_id():
+    return random.randint(1001, 2000)  # випадкове _id після 1000
 
-client_local.close()
-client_atlas.close()
+# Створення записів у першій колекції з посиланнями на документи у другій колекції
+for i in range(2000):
+    document = {
+        '_id': i + 1,  # починаючи з 1
+        'counter': i,
+        'name': f'name_{i}'  # поле name
+    }
+    # Випадкове _id документа з другої колекції
+    if i >= 999:  # вставляємо випадкові _id після 1000 елемента
+        document['ref_id'] = generate_random_id()
+    collection1.insert_one(document)
+
+# Створення записів у другій колекції з посиланнями на документи у першій колекції
+for i in range(2000):
+    document = {
+        '_id': i + 1001,  # починаючи з 1001
+        'counter': i,
+        'name': f'name_{i}'  # поле name
+    }
+    # Випадкове _id документа з першої колекції
+    if i >= 999:  # вставляємо випадкові _id після 1000 елемента
+        document['ref_id'] = random.randint(1, 1000)  # випадкове _id з першої колекції
+    collection2.insert_one(document)
+
+print("Колекції створені успішно.")
